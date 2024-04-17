41541c4b72e4a5e25d0a63c495d15b103fe686e3
Israaeel <Israelrock@hotmail.com>
2024-04-17 19:37:42 -0300

    adding POJOs. Closes #2.

---
+++ src/main/java/israel/springframework/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package israel.springframework.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/israel/springframework/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package israel.springframework.sfgpetclinic.model;
+
+public class Vet extends Person {
+}
